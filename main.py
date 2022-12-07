f9d0d6c498e44e4de0cfc3b50c91e687045f2b24
khaingovan <khaingovan777@gmail.com>
2022-12-07 21:39:58 +0700

    hello

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pygame, time, random
+import pygame, time
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, wdoheo))
+screen = pygame.display.set_mode((500,500))
 win = pygame.display.set_mode((339, 333))
 
 font = pygame.font.SysFont("Times New Roman", 20, True)
@@ -10,11 +10,12 @@ text = font.render("AAABBBCCC", True, (255, 255, 255))
 screen.fill((0, 0, 0))
 screen.blit(text, (0, 0))
 
-    #rbvtwlrbvgebvt
+
 
 
 running = True
 while running:
+    #print("x")git
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
