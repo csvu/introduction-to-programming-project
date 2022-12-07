7ae844368f1b74945d86dd92fbba491afd659c82
khaingovan <khaingovan777@gmail.com>
2022-12-07 13:04:48 +0700

    doi khung 750 thanh 450

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, time
 
 pygame.init()
 
-screen = pygame.display.set_mode((450, 750))
+screen = pygame.display.set_mode((450, 450))
 font = pygame.font.SysFont("Calibri", 20, True)
 text = font.render("AAA", True, (255, 255, 255))
 screen.fill((0, 0, 0))
