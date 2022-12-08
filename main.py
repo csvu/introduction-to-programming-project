10a0d8681edcf5512b1a1e064dc1e8e25bf40100
khaingovan <khaingovan777@gmail.com>
2022-12-08 19:44:39 +0700

    hello

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import pygame, time
 
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
-win = pygame.display.set_mode((339, 333))
+screen = pygame.display.set_mode((500,700))
+win = pygame.display.set_mode((400, 700))
+
+#RGB color values
+black = (0, 0, 0)
+gray = (127, 127, 127)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+
 
 font = pygame.font.SysFont("Times New Roman", 20, True)
-text = font.render("AAABBBCCC", True, (255, 255, 255))
+text = font.render("AAABBBCCC", True, white)
 screen.fill((0, 0, 0))
 screen.blit(text, (0, 0))
 
