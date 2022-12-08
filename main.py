727006ca3284e4bc8829eb9243d85baba3b2db1a
khaingovan <khaingovan777@gmail.com>
2022-12-08 19:47:09 +0700

    add colors

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ screen = pygame.display.set_mode((500,700))
 win = pygame.display.set_mode((400, 700))
 
 #RGB color values
-black = (0, 0, 0)
-gray = (127, 127, 127)
-white = (255, 255, 255)
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-yellow = (255, 255, 0)
-cyan = (0, 255, 255)
+black  = (  0,   0,   0)
+gray   = (127, 127, 127)
+white  = (255, 255, 255)
+red    = (255,   0,   0)
+green  = (  0, 255,   0)
+blue   = (  0,   0, 255)
+yellow = (255, 255,   0)
+cyan   = (  0, 255, 255)
 
 
 font = pygame.font.SysFont("Times New Roman", 20, True)
